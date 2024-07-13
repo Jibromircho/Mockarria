49ee1d60cd604a90792048336849e765f7b36e8a
Jibromircho <Jibromircho@gmail.com>
2024-07-13 08:31:59 +0100

    added simple gravity and a jump

--- build/CMakeFiles/Mockarria.dir/src/main.cpp.o.d
+++ build/CMakeFiles/Mockarria.dir/src/main.cpp.o.d
@@ -1,6 +1,8 @@
 CMakeFiles/Mockarria.dir/src/main.cpp.o: \
- /home/didi/code/Mockarria/src/main.cpp /usr/include/stdc-predef.h \
- /home/didi/code/Mockarria/include/config.h /usr/include/c++/11/iostream \
+ /home/didi-desktop/Code/Mockarria/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/didi-desktop/Code/Mockarria/include/config.h \
+ /usr/include/c++/11/iostream \
  /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/os_defines.h \
  /usr/include/features.h /usr/include/features-time64.h \
@@ -146,5 +148,5 @@ CMakeFiles/Mockarria.dir/src/main.cpp.o: \
  /usr/include/c++/11/bits/basic_ios.tcc \
  /usr/include/c++/11/bits/ostream.tcc /usr/include/c++/11/istream \
  /usr/include/c++/11/bits/istream.tcc \
- /home/didi/code/Mockarria/include/raylib.h \
- /home/didi/code/Mockarria/include/player.h
+ /home/didi-desktop/Code/Mockarria/include/raylib.h \
+ /home/didi-desktop/Code/Mockarria/include/player.h
